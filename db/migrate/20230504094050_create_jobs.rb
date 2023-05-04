de9ed9046062cf72d77347ea260b7825f0942ff4
Malak ALshmali <79609843+Malakalshamali@users.noreply.github.com>
2023-05-04 12:49:32 +0300

    -

---
+++ db/migrate/20230504094050_create_jobs.rb
@@ -0,0 +1,16 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :description
+      t.string :company
+      t.string :country
+      t.string :city
+      t.string :job_type
+      t.text :requirements
+      t.datetime :published_at
+      t.datetime :expires_at
+      t.timestamps
+    end
+  end
+end
