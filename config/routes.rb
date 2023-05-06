ef5d5733851aa2673e25a81e8f447c3837d286a1
Malak ALshmali <79609843+Malakalshamali@users.noreply.github.com>
2023-05-06 19:54:35 +0300

    job_application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :jobs
+  resources :users
+  resources :jobs do
+    resources :job_applications do
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_094050) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_06_160031) do
+  create_table "job_applications", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "resume"
+    t.text "cover_letter"
+    t.string "status", default: "NOT_SEEN"
+    t.integer "user_id", null: false
+    t.integer "job_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_job_applications_on_job_id"
+    t.index ["user_id"], name: "index_job_applications_on_user_id"
+  end
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -24,4 +39,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_094050) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_users_on_role_id"
+  end
+
+  add_foreign_key "job_applications", "jobs"
+  add_foreign_key "job_applications", "users"
+  add_foreign_key "users", "roles"
 end
